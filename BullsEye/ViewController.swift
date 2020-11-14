13eb436b7925b801cf01a31b9cf2ecdbd56017e2
leon walras <leongdev@gmail.com>
2020-11-14 14:15:24 -0300

    Adding initial functions

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert(){
+        
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: "The slider value is: \(currentValue)",
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil
+        )
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider:UISlider){
+        currentValue = Int(slider.value)
+    }
+    
 }
-
