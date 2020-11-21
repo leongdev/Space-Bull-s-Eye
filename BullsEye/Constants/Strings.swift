41b37764043f103fe12a68bd5ebe92c3bf514e29
leon walras <leongdev@gmail.com>
2020-11-21 19:59:33 -0300

    Cleaning the code

--- BullsEye/Constants/Strings.swift
+++ BullsEye/Constants/Strings.swift
@@ -8,9 +8,6 @@
 import Foundation
 struct Strings {
     struct GameScreen {
-        static let scoreModalLabel: String = "Score"
-        static let bullsEyeModalLabel: String = "Bull's Eye"
-        static let playerPositionModalLabel: String = "Yout Position"
         static let startGameInfoLabel: String = "Press Play to Start"
         static let sortedValueLabel: String = "Stop at: "
     }
